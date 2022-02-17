ee1eba01f69830efddcbd9d16c8feae4acbc9e69
ankitraj141 <92325243+ankitraj141@users.noreply.github.com>
2022-02-17 21:03:54 +0530

    project 4 task 2
    Write a Python program to print all positive numbers in a range.

---
+++ AI_project_task_4(2).py
@@ -0,0 +1,13 @@
+list1 = [12, -7, 5, 64, -14]
+# For second list 
+for n in list1:
+    if n >= 0:
+       print(n, end= " ")
+print("\n")    
+
+# For second list    
+list2 = [12, 14, -95, 3]
+for n in list2:
+    if n >= 0:
+      
+       print(n, end = " ")
